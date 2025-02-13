cce5fb14569db1b53ce181b40b527ddb06b8bc55
webDev <kritimisra67@gmail.com>
2025-02-13 12:02:30 +0530

    Initial Commit

--- Addtask/script.js
+++ Addtask/script.js
@@ -2,16 +2,19 @@
 let taskInput = document.getElementById("taskInput");
 let addTaskButton = document.getElementById("addTask");
 let taskList = document.getElementById("taskList");
-
+let taskText=taskInput.value.trim; //To access everything in the element 
 // Function to add a task
 function addTask() {
 
+    let li=document.createElement("li");
+    li.textContent=taskText;
+    taskList.appendChild(li);
     // take task text
 
     // Append task
-
+    taskInput.value=""
     // Clear input
 }
 
 // Event Listener
-// addTaskButton.addEventListener("click", addTask);
+addTaskButton.addEventListener("click", addTask);
